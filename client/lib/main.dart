6f1e60539307be2bd03d33312b7b93937c248533
Marti Olle <martiollea@gmail.com>
2019-12-09 18:31:22 +0100

    Modificacions

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,8 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:client/MainPage.dart';
+import 'package:client/Notifications.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -10,9 +12,13 @@ class MyApp extends StatelessWidget {
     final db= Firestore.instance;
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue,),
+
+      routes: {
+        '/': (context) => MainPage(),
+        '/Notifications': (context) => Notifications(),
+      },
+
       home: Scaffold(
         body: FutureBuilder(
           future: db.collection('todos').getDocuments(),
