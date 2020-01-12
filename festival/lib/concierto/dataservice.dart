cbbd1e7716cffb3750eef1f97dc3fb01f31e3f52
Marti Olle <martiollea@gmail.com>
2020-01-12 02:01:18 +0100

    Main

---
+++ festival/lib/concierto/dataservice.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:flutter/cupertino.dart';
+
+class DatabaseService {
+
+  final String codigoIDConcierto;
+  DatabaseService({this.codigoIDConcierto});
+
+  final CollectionReference concertCollection = Firestore.instance.collection('concierto');
+
+  Future updateConcertData(String codigoIDConcierto, String codigoIDGrupo, String imagenGrupo, String escenario, String dia, String horaInicio, String horaFinal) async {
+    return await concertCollection.document(codigoIDConcierto).setData({
+        "codigoIDConcierto": codigoIDConcierto,
+        "codigoIDGrupo": codigoIDGrupo,
+        "imagenGrupo": imagenGrupo, //'assets/LP.jpg',
+        "dia": dia,
+        "horaInicio": horaInicio,
+        "horaFinal": horaFinal,
+        "escenario": escenario,
+      }
+    );
+  }
+}
--- festival/lib/main.dart
+++ festival/lib/main.dart
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-//import 'package:festival/escenario/stages.dart';
-=======
-import 'package:festival/concierto/concerts.dart';
-import 'package:festival/concierto/concierto.dart';
-import 'package:festival/escenario/stages.dart';
->>>>>>> c34232149c8924941ce373efcdc210f117a443ed
 import 'package:flutter/material.dart';
 //import 'package:festival/grupo/infoGroup.dart';
 
 void main(){
-<<<<<<< HEAD
   runApp(MyApp());
-=======
-  runApp(Conciertos());
->>>>>>> c34232149c8924941ce373efcdc210f117a443ed
 }
 
 class MyApp extends StatelessWidget {
